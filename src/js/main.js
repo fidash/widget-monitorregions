d5f9d83bc00c946de996e1b8dd21bf7daa674dc7
bgrana <bgrana@conwet.com>
2015-06-30 19:30:57 +0200

    Use google charts to show monitoring stats

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {
+/* global Monitoring,google */
+
+(function() {
     "use strict";
 
-    // Load the Visualization API and the piechart package.
-    google.load('visualization', '1.0', {'packages':['corechart']});
+    var monitoring = new Monitoring();
+    monitoring.init();
 
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(Monitoring.drawChart);
-
-});
+})();
