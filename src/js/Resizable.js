2d25d3bebed9d9bb7c894a584016c3ff2b795c54
bgrana <bgrana@conwet.com>
2015-07-07 11:42:35 +0200

    Charts stop resizing when past the minimum width or height

--- src/js/Resizable.js
+++ src/js/Resizable.js
@@ -1,9 +1,17 @@
 var Resizable  = (function () {
     "use strict";
 
+    var MIN_WIDTH = 543;
+    var MIN_HEIGHT = 382;
+
     function Resizable () {}
 
     Resizable.prototype.resize = function (charts, newValues) {
+
+        if (window.innerWidth < MIN_WIDTH || window.innerHeight < MIN_HEIGHT) {
+            return;
+        }
+
         if ("heightInPixels" in newValues || "widthInPixels" in newValues) {
             for (var type in charts) {
                 var current = charts[type];
